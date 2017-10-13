e00eaf24f8786c5cbe921d5ca037544634cfd304
Bernard Van De Walle <bernard@bvandewa.com>
2017-10-12 19:56:53 -0700

    New: Support for a buffered model for non-blocking calls from Trireme (#14)
    * New: Support for a buffered model for non-blocking calls from Trireme
    
    * Fix: Style and remocing useless methods//params
    
    * Removing all the channels
    
    * CollectFlow and CollectContainer now add to channel
    
    * Renaming interface and struct type

--- collector/influxdb/influxdb.go
+++ collector/influxdb/influxdb.go
@@ -4,73 +4,66 @@ import (
 	"fmt"
 	"time"
 
-	"go.uber.org/zap"
-
 	tcollector "github.com/aporeto-inc/trireme/collector"
 	"github.com/influxdata/influxdb/client/v2"
 )
 
+// TODO: Remove default credentials
 const (
 	database = "flowDB"
 	username = "aporeto"
 	password = "aporeto"
 )
 
+//Influxdb inplements a DataAdder interface for influxDB
+type Influxdb struct {
+	httpClient client.Client
+
+	worker     *worker
+	stopWorker chan struct{}
+}
+
+//DataAdder interface has all the methods required to interact with influxdb api
+type DataAdder interface {
+	CreateDB() error
+	AddData(tags map[string]string, fields map[string]interface{}) error
+}
+
 // NewDB is used to create a new client and return influxdb handle
-func NewDB(user string, pass string, addr string) (*Influxdbs, error) {
+func NewDB(user string, pass string, addr string) (*Influxdb, error) {
 	httpClient, err := createHTTPClient(user, pass, addr)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Unable to create InfluxDB http client %s", err)
 	}
-	return &Influxdbs{
-		httpClient:  httpClient,
-		reportFlows: make(chan map[string]interface{}),
-		stop:        make(chan bool),
-		doneAdding:  make(chan bool),
-		tags:        make(chan map[string]string),
+
+	return &Influxdb{
+		httpClient: httpClient,
 	}, nil
 }
 
 func createHTTPClient(user string, pass string, addr string) (client.Client, error) {
-	if user == "" && pass == "" || addr == "" {
-		httpClient, err := client.NewHTTPClient(client.HTTPConfig{
-			Addr:     "http://influxdb:8086",
-			Username: username,
-			Password: password,
-		})
 
-		if err != nil {
-			return nil, err
-		}
-		return httpClient, nil
+	// TODO: Remove this.
+	if user == "" && pass == "" || addr == "" {
+		addr = "http://influxdb:8086"
+		user = username
+		pass = password
 	}
+
 	httpClient, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:     addr,
 		Username: user,
 		Password: pass,
 	})
-
 	if err != nil {
 		return nil, err
 	}
+
 	return httpClient, nil
-
-}
-
-// CreateAndConnectDB is used by the caller to connect with th ecurrent instance and start collecting
-func CreateAndConnectDB(user string, pass string, addr string) *Influxdbs {
-	httlpcli, err := NewDB(user, pass, addr)
-	if err != nil {
-		zap.L().Fatal("Failed to connect", zap.Error(err))
-	}
-
-	httlpcli.Start()
-
-	return httlpcli
 }
 
 // CreateDB is used to create a new databases given name
-func (d *Influxdbs) CreateDB(dbname string) error {
+func (d *Influxdb) CreateDB(dbname string) error {
 	if dbname == "" {
 		q := client.NewQuery("CREATE DATABASE "+database, "", "")
 		if response, err := d.httpClient.Query(q); err != nil && response.Error() != nil {
@@ -87,142 +80,65 @@ func (d *Influxdbs) CreateDB(dbname string) error {
 	return nil
 }
 
-// AddToDB is used to add points to the data as batches
-func (d *Influxdbs) AddToDB(tags map[string]string, fields map[string]interface{}) error {
-
-	if fields != nil {
-		d.reportFlows <- fields
-		d.tags <- tags
-		if <-d.doneAdding {
-			err := d.httpClient.Write(d.batchPoint)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
 // Start is used to start listening for data
-func (d *Influxdbs) Start() error {
-
-	// d.grafana, err = grafana.LaunchGrafanaCharts()
-	// if err != nil {
-	// 	return err
-	// }
-
-	go d.listen()
+func (d *Influxdb) Start() error {
+	go d.worker.startWorker()
 
 	return nil
 }
 
 // Stop is used to stop and return from listen goroutine
-func (d *Influxdbs) Stop() error {
-	<-d.stop
+func (d *Influxdb) Stop() error {
+	d.stopWorker <- struct{}{}
 	d.httpClient.Close()
+
 	return nil
 }
 
-func (d *Influxdbs) listen() {
-
-	for {
-		select {
-		case r := <-d.reportFlows:
-			go func(r map[string]interface{}) {
-				d.AddData(<-d.tags, r)
-			}(r)
-		case <-d.stop:
-			return
-		default:
-		}
-	}
-}
-
 // AddData is used to add data to the batch
-func (d *Influxdbs) AddData(tags map[string]string, fields map[string]interface{}) {
+func (d *Influxdb) AddData(tags map[string]string, fields map[string]interface{}) error {
 	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
 		Database:  database,
 		Precision: "us",
 	})
 	if err != nil {
-
+		return fmt.Errorf("Couldn't add data: %s", err)
 	}
+
 	if tags["EventName"] == "ContainerStartEvents" || tags["EventName"] == "ContainerStopEvents" {
 		pt, err := client.NewPoint("ContainerEvents", tags, fields, time.Now())
 		if err != nil {
 			fmt.Println(err)
 		}
-		zap.L().Info(pt.String())
 		bp.AddPoint(pt)
 	} else if tags["EventName"] == "FlowEvents" {
 		pt, err := client.NewPoint("FlowEvents", tags, fields, time.Now())
 		if err != nil {
 			fmt.Println(err)
 		}
-		zap.L().Info(pt.String())
 		bp.AddPoint(pt)
 	}
-	d.batchPoint = bp
-	d.doneAdding <- true
+	if err := d.httpClient.Write(bp); err != nil {
+		return fmt.Errorf("Couldn't add data: %s", err)
+	}
 }
 
 // CollectFlowEvent implements trireme collector interface
-func (d *Influxdbs) CollectFlowEvent(record *tcollector.FlowRecord) {
-	//	cid, _ := d.cache.Get(record.ContextID)
-	//if record.ContextID == cid {
-	//d.grafana.CreateGraphs(grafana.Graph, "events", "Action", "FlowEvents")
-
-	d.AddToDB(map[string]string{
-		"EventName": "FlowEvents",
-		"EventID":   record.ContextID,
-	}, map[string]interface{}{
-		"ContextID":       record.ContextID,
-		"Counter":         record.Count,
-		"SourceID":        record.Source.ID,
-		"SourceIP":        record.Source.IP,
-		"SourcePort":      record.Source.Port,
-		"SourceType":      record.Source.Type,
-		"DestinationID":   record.Destination.ID,
-		"DestinationIP":   record.Destination.IP,
-		"DestinationPort": record.Destination.Port,
-		"DestinationType": record.Destination.Type,
-		"Action":          record.Action,
-		"DropReason":      record.DropReason,
-		"PolicyID":        record.PolicyID,
-	})
-	//} else {
-	//fmt.Println("NO PU RUNNING BUT RECEIVED A FLOW")
-	//}
+func (d *Influxdb) CollectFlowEvent(record *tcollector.FlowRecord) {
+	d.worker.addEvent(
+		&workerEvent{
+			event:      flowEvent,
+			flowRecord: record,
+		},
+	)
 }
 
 // CollectContainerEvent implements trireme collector interface
-func (d *Influxdbs) CollectContainerEvent(record *tcollector.ContainerRecord) {
-	if record.Event == "start" {
-		//d.cache.Add(record.ContextID, record.ContextID)
-		//d.contextID = record.ContextID
-		//d.grafana.AddRows(grafana.Graph, "events", "Action", "FlowEvents")
-		d.AddToDB(map[string]string{
-			"EventName": "ContainerStartEvents",
-			"EventID":   record.ContextID,
-		}, map[string]interface{}{
-			"ContextID": record.ContextID,
-			"IPAddress": record.IPAddress,
-			"Tags":      record.Tags,
-			"Event":     record.Event,
-		})
-	}
-	if record.Event == "delete" {
-		//d.cache.Add(record.ContextID, record.ContextID)
-		//d.contextID = record.ContextID
-		//d.grafana.AddRows(grafana.Graph, "events", "Action", "FlowEvents")
-		d.AddToDB(map[string]string{
-			"EventName": "ContainerStopEvents",
-			"EventID":   record.ContextID,
-		}, map[string]interface{}{
-			"ContextID": record.ContextID,
-			"IPAddress": record.IPAddress,
-			"Tags":      record.Tags,
-			"Event":     record.Event,
-		})
-	}
+func (d *Influxdb) CollectContainerEvent(record *tcollector.ContainerRecord) {
+	d.worker.addEvent(
+		&workerEvent{
+			event:           containerEvent,
+			containerRecord: record,
+		},
+	)
 }
---
+++ collector/influxdb/worker.go
@@ -0,0 +1,109 @@
+package influxdb
+
+import collector "github.com/aporeto-inc/trireme/collector"
+
+// A worker manages the workload for the InfluxDB collector
+type worker struct {
+	events chan *workerEvent
+	stop   chan struct{}
+	db     DataAdder
+}
+
+type eventType int
+
+const (
+	containerEvent eventType = iota
+	flowEvent      eventType = iota
+)
+
+// a workerEvent is an event that the worker need to process
+type workerEvent struct {
+	event           eventType
+	containerRecord *collector.ContainerRecord
+	flowRecord      *collector.FlowRecord
+}
+
+func newWorker(stop chan struct{}, db DataAdder) *worker {
+	return &worker{
+		events: make(chan *workerEvent, 100),
+		stop:   stop,
+		db:     db,
+	}
+}
+
+func (w *worker) addEvent(wevent *workerEvent) {
+	w.events <- wevent
+}
+
+// startWorker start processing the event for this worker.
+// Blocking... Use go.
+func (w *worker) startWorker() {
+	for {
+		select {
+		case event := <-w.events:
+			w.processEvent(event)
+		case <-w.stop:
+			return
+		}
+	}
+}
+
+func (w *worker) processEvent(wevent *workerEvent) {
+	switch wevent.event {
+	case containerEvent:
+		w.doCollectContainerEvent(wevent.containerRecord)
+
+	case flowEvent:
+		w.doCollectFlowEvent(wevent.flowRecord)
+
+	}
+}
+
+// CollectContainerEvent implements trireme collector interface
+func (w *worker) doCollectContainerEvent(record *collector.ContainerRecord) {
+	if record.Event == "start" {
+		w.db.AddData(map[string]string{
+			"EventName": "ContainerStartEvents",
+			"EventID":   record.ContextID,
+		}, map[string]interface{}{
+			"ContextID": record.ContextID,
+			"IPAddress": record.IPAddress,
+			"Tags":      record.Tags,
+			"Event":     record.Event,
+		})
+	}
+
+	if record.Event == "delete" {
+		w.db.AddData(map[string]string{
+			"EventName": "ContainerStopEvents",
+			"EventID":   record.ContextID,
+		}, map[string]interface{}{
+			"ContextID": record.ContextID,
+			"IPAddress": record.IPAddress,
+			"Tags":      record.Tags,
+			"Event":     record.Event,
+		})
+	}
+}
+
+// CollectFlowEvent implements trireme collector interface
+func (w *worker) doCollectFlowEvent(record *collector.FlowRecord) {
+	w.db.AddData(map[string]string{
+		"EventName": "FlowEvents",
+		"EventID":   record.ContextID,
+	}, map[string]interface{}{
+		"ContextID":       record.ContextID,
+		"Counter":         record.Count,
+		"SourceID":        record.Source.ID,
+		"SourceIP":        record.Source.IP,
+		"SourcePort":      record.Source.Port,
+		"SourceType":      record.Source.Type,
+		"DestinationID":   record.Destination.ID,
+		"DestinationIP":   record.Destination.IP,
+		"DestinationPort": record.Destination.Port,
+		"DestinationType": record.Destination.Type,
+		"Action":          record.Action,
+		"DropReason":      record.DropReason,
+		"PolicyID":        record.PolicyID,
+	})
+}
